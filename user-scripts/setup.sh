d047c300998132d58a352dcbc06ff60ac2c928ce
Eduardo Garibay <dark.cloud2007@gmail.com>
2024-02-07 21:54:19 -0600

    Updates setup.sh
    * Adds guard for .env variables
    * Adds option to pass configuration to `.bashrc` if `.env` is present

--- user-scripts/setup.sh
+++ user-scripts/setup.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+if [ -n "${SERVER_NAME}" ]; then
+  # Assuming that if this is defined, this was already ran once before
+  echo "\$SERVER_NAME is already configured, refusing to run"
+  exit 0
 fi
 
+if [ -f "${HOME}/.env" ]; then
+  # Adds script to load .env variables to .bashrc
+  cat<<EOF >> "$HOME/.bashrc"
+if [ -f "${HOME}/.env" ]; then
+  export $(grep -v '^#' "${HOME}/.env" | xargs)
+fi
+EOF
+
+  # First time running will not have the env variables
+  export $(grep -v '^#' "${HOME}/.env" | xargs)
+fi
+
+# Fallback for if `.env` is not configured
 SERVER_NAME=${SERVER_NAME:-"PiHeim"}
 
 # Download steam
